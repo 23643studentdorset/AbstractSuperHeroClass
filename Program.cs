c541697c4859006e05633b73e955be4f737fb390
23643studentdorset <23643@student.dorset-college.ie>
2021-11-09 17:43:29 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using AbstractSuperHeroClass.models;
+using AbstractSuperHeroWithInterface01.Models;
+using System;
+
+namespace AbstractSuperHeroClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperMan superman = new SuperMan ();
+            superman.Fly();
+            superman.LookTroughObjects();
+
+            Sword sword = new Sword("BatSword");
+            sword.TakeDamage(5);
+
+        }
+    }
+}
---
+++ models/Various.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractSuperHeroClass.models
+{
+
+    public abstract class Superhero
+    {
+
+    }
+    public abstract class Alien: Superhero
+    {
+
+    }
+    public abstract class Human: Superhero
+    {
+
+    }
+    
+    interface IFly
+    {
+        void Fly();
+    }
+    interface ILookTroughObjects
+    {
+        void LookTroughObjects();
+    }
+
+    
+    
+    
+    
+    public class SuperMan: Alien, ILookTroughObjects, IFly
+    {
+        public void LookTroughObjects()
+        {
+            Console.WriteLine("X-RAY");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("With no limits");
+        }
+
+    }
+
+
+    
+   
+}
